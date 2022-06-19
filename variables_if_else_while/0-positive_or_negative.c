9463c4e5d551f6445989d6e922616c0785b7c40b
Giogap <giogap@gmail.com>
2022-06-18 20:35:13 -0700

    pnz

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main block
+ * print the number
+ * positive, zero, negative
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
