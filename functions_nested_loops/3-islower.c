c83456b521c590aae2fa00e8c8805496a1054103
Giogap <giogap@gmail.com>
2022-06-20 15:03:48 -0700

    1c-0C

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower  - entry point
+ *
+ * Description: Prints the alphabet
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	
+		return (1);
+	else
+		return (0);
+}
