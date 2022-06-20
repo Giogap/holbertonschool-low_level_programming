712d65f215249032a1eef204ca5939c75dcd6028
Giogap <giogap@gmail.com>
2022-06-20 10:01:14 -0700

    0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  main alfabeto
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+
+	printf(x);
+
+	printf("\n");
+
+	return (0);
+}
