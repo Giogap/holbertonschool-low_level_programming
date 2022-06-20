10d65c065a7d08db2dbbade68d57d0a85dc62f60
Giogap <giogap@gmail.com>
2022-06-20 13:22:06 -0700

    abc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+
+		_putchar(l);
+
+	putchar('\n');
+
+	print_alphabet();
+    	return (0);
+
+}
