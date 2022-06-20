b5110c45cf05e5e913e219fcb46b25e9e42ced2f
Giogap <giogap@gmail.com>
2022-06-20 09:17:09 -0700

    abcd

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  main alfabeto
+ * 
+ *  
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l;
+	for(l = 'a'; l <= 'z'; l++)
+
+		putchar(l);
+
+	putchar('\n');
+
+		return (0);	
+}
