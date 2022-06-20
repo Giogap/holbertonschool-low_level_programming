97132cf2adb651bbff1079c6a6c85aea47709e18
Giogap <giogap@gmail.com>
2022-06-20 10:13:14 -0700

    0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  main num
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 0; x < 10; x++)
+
+	putchar("%d",  x);
+
+	putchar('\n');
+
+	return (0);
+}
