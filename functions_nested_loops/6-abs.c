75513003dde829d3a500ceb8dd269ffdb48cfb2f
Giogap <giogap@gmail.com>
2022-06-20 16:30:55 -0700

    absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - checks
+ *
+ * @n: absolute
+ *
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	else
+		return (n);
+}
