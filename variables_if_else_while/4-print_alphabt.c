53b958a8e55cee2979a2d6884c5d24a7feac3a88
Giogap <giogap@gmail.com>
2022-06-20 09:51:52 -0700

    abc-eq

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  main alfabeto
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+
+		if (l != 'e' && l != 'q')
+
+		putchar(l);
+
+	putchar('\n');
+
+		return (0);
+}
