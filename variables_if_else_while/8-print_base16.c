5ddfc4e85b4b073695ddcb22f94bef92a0c8dc79
Giogap <giogap@gmail.com>
2022-06-20 10:49:23 -0700

    123-abc

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -  main num
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	char l;
+
+	for (x = 0; x < 10; x++)
+
+	putchar((x % 16) + '0');
+
+	for (l = 'a'; l <= 'z'; l++)
+
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
