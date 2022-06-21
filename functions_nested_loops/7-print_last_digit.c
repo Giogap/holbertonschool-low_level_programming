1be88b404d082dcd5eb64b60ea474637b1c660ca
Giogap <giogap@gmail.com>
2022-06-20 17:07:53 -0700

    last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _islower - checks
+ * print_last_digit - checks
  *
- * @c: character to check
+ * @n: character to check
  *
- * Return: 0 or 1
+ * Return: n
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
-
+	n = n % 10;
+	return (n);
 }
