7c7e39f8ea78ce59632e542baaa3c6cb4937c00c
Giogap <giogap@gmail.com>
2022-06-20 20:00:11 -0700

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,13 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * add - check the code
- *
- * @x - n1
- * @y - n2
- *
+ * add - a function 
+ * @x: first number 
+ * @y: second number
  * Return: x + y
  */
-
 int add(int x, int y)
 {
 	return (x + y);
