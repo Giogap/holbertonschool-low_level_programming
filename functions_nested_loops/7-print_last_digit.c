f643795493b538569e66c4d2ecf3885bb47aaca7
Giogap <giogap@gmail.com>
2022-06-20 17:28:51 -0700

    last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,7 @@ int print_last_digit(int n)
 	{
 		n = n * -1;
 		x = n % 10;
-		_putchar(x + '0'); 
+		_putchar(x + '0');
 		return (x);
 	}
 	else
@@ -22,5 +22,5 @@ int print_last_digit(int n)
 		x = n % 10;
 		_putchar(x + '0');
 		return (x);
-	}	
+	}
 }
