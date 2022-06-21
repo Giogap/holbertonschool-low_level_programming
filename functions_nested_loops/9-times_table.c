65d3ce5786bea771df987e97dd678f0d95cff6c5
Giogap <giogap@gmail.com>
2022-06-20 19:24:48 -0700

    nueve

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,10 +9,33 @@ void times_table(void)
 {
 	int t;
 	int i;
+	int j;
 
-	for (i = 0; i < 11; i++)
-	{
-		t = 9 * i;
-		_putchar(t + '0'); 
+	for (i = 0; i < 10; i++)
+	{	
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 0; j < 10; j++)
+		{
+			t = i * j;
+			if ((t / 10) > 0)
+			{
+				_putchar((t / 10) + '0');
+			}
+			else
+			{
+				_putchar (' ');
+			}
+			_putchar((t % 10) + '0' );
+		
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}		
+		_putchar('\n');
 	}
 }
