c1c392f94624fe35561a82ff06703679257c5c18
Giogap <giogap@gmail.com>
2022-06-22 16:10:15 -0700

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_most_numbers - no print 2 - 4
+*
+* Description: numbers 0 - 9
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
