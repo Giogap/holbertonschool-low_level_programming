9eea75e30a037feae5ef3bd95f79ee72cb51300b
Giogap <giogap@gmail.com>
2022-06-22 09:22:37 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *
+ * @c: int
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
