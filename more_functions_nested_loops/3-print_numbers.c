40bb9d899d595f6dfb628f19ddaf85f53fe414aa
Giogap <giogap@gmail.com>
2022-06-22 15:05:36 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
