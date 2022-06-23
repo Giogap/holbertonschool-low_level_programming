7f2c7dca82edfb1439c9905d30f462e2a94d5813
Giogap <giogap@gmail.com>
2022-06-22 19:46:29 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - check code
+*
+* @n: num_
+*
+* Description: print ____
+*/
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
