1cc4402dd08d3083e9e0ed512d1abe8e9880db39
Giogap <giogap@gmail.com>
2022-06-22 20:44:26 -0700

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* print_diagonal - check code
+*
+* Description: print ///
+*/
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (n > 0)
+	{
+		b = a;
+		while (a > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+
+		a++;
+		b--;
+	}
+	if (c < 1)
+	{
+		_putchar('\n');
+	}
+}
