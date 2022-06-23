688d65a6be1641f1b7dbbd0338d92b6735820f62
Giogap <giogap@gmail.com>
2022-06-22 18:52:26 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - function to print numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int y;
+	int x;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			if (x > 9)
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
