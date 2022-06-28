44b8a9771a2d3374af452914928126bedb9c63d3
Giogap <giogap@gmail.com>
2022-06-28 16:18:17 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - check the code
+ *
+ * @str: string to change
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int c = 2;
+
+	for (i = 0; i != 0; i++)
+	{
+		if (c % 2 == 0)
+		{
+			puts(str[i]);
+			c++;
+		}
+	}
+}
