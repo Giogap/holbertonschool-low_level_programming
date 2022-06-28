0fef5afe0c797a6fa35f8bdcda71e8cd1480ba91
Giogap <giogap@gmail.com>
2022-06-28 15:50:16 -0700

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,14 +12,19 @@ void rev_string(char *s)
 {
 	int i;
 	int c = -1;
+	char st[10];
 
 	for (i = 0; s[i] != 0; i++)
 	{
+		st[i] = s[i];
 		c = c + 1;
 	}
+	i = 0;
+
 	while (c >= 0)
 	{
+		s[i] = st[c];
+		i++;
 		c--;
 	}
-	_putchar('\n');
 }
