efc5f84096f1c85095d58e813702375fcd716a46
Giogap <giogap@gmail.com>
2022-06-29 12:59:33 -0700

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -17,9 +17,9 @@ char *_strcat(char *dest, char *src)
 	{
 		i = i + 1;
 	}
-	while (scr[j] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[i] = scr[j];
+		dest[i] = src[j];
 			i = i + 1;
 			j = j + 1;
 	}
