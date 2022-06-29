9ee5d24af8c4969bc1fd350b14793aa27f205721
Giogap <giogap@gmail.com>
2022-06-28 19:31:13 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,36 @@
-#
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * @str: string to change
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int c = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		c = c + 1;		
+	}
+	j = c / 2;
+
+	if (j % 2 == 0)
+	{
+		while (str[j] != '\0')
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+		c = c - 1;
+		j = c / 2;
+
+		while (str[j] != '\0')
+		{
+			_putchar(str[j]);
+		}
+}
