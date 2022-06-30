6511d24a55944dab0fe0af1be977fa4fbac2ea68
Giogap <giogap@gmail.com>
2022-06-29 19:01:49 -0700

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - check the code
+ * @dest: string one
+ * @src: add string to string one
+ * Description: add src to dest
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j = 0;
+
+	while (dest[j] != '\0')
+	{
+		j = j + 1;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+		j = j + 1;
+	}
+	return (dest);
+}
