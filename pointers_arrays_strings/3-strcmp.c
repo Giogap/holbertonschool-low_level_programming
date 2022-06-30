eb07055b4d26dd0f6c38b728e1847305de320d8e
Giogap <giogap@gmail.com>
2022-06-29 22:09:35 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
+	int j = 0;
 
-	for (i = 0; s1[i] == s2[i]; i++)
-		
-		return (s1[i]);
-		return (s2[i]);
-	
+	while (s1[i] != '\0' && j == 0)
+	{
+		j = s1[i] - s2[i];
+		i++;
+	}
+	return (j);
 }
