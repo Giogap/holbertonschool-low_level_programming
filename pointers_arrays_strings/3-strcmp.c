6cce397ebdacc3cdcf61a2146dae97e36c7dcb8c
Giogap <giogap@gmail.com>
2022-06-29 21:41:56 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - check the code
+ * @s1: compare with s2
+ * @s2: compare with s1
+ * 
+ * Description: compare
+ * Return: s1 - s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+		
+		return (s1[i]);
+		return (s2[i]);
+	
+}
