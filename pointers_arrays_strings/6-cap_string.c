ba4109471a3d896885512d88aa053879818a3de3
Giogap <giogap@gmail.com>
2022-06-30 21:06:35 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,22 +12,27 @@
 
 char *cap_string(char *m)
 {
-	int i = 0;
-	int j = 1;
-	char *n = " \t\n;,.!?\"(){}\0";
+	int i;
+	int j;
+	int k = 0;
+	char *c = " \t\n;,.!?\"(){}\0";
 
-	while (m[i] != '\0')
-	{		
-		if (j = 1 && m[i] >= 97 && m[i] <= 122)
-		{	
-			m[i] = m[i] - 32;
-			j++;
-		}
-		for (i = 0; n[i]; i++)
-		{
-			if (m[i] == n[i])
-				j--;;
-		}
-		i++;
+	if (m[k] >= 97 && m[k] <= 122)
+	{
+		m[k] = m[k] - 32;
 	}
+	for (i = 0; m[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (m[i] == c[j])
+			{
+				if (m[i + 1] >= 97 && m[i + 1] <= 122)
+				{
+					m[i + 1] = m[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (m);
 }
