96f3476ccedd68eca49577174566f81c4a0f9d63
Giogap <giogap@gmail.com>
2022-07-02 10:16:30 -0700

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,25 +2,26 @@
 #include <stdio.h>
 
 /**
- * _memset - check the code
+ * _strchr - check the code
  *
  * @s: array
- * @b: const
- * @n: # dat
- * Description: change for #s
+ * @c: comp
  *
+ * Description: iqual or no
  * Return: s
  */
 
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
+		{
 			return (s);
-		else if (s[i + 1]== c)
+		}
+		else if (s[i + 1] == c)
 		{
 			return (s + 1);
 		}
