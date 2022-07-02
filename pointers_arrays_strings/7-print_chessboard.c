7274387b3504db3786e31a6e25d78d603ae0efb8
Giogap <giogap@gmail.com>
2022-07-02 15:30:10 -0700

    6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - check code
+ * @s: cadena 1
+ * @accept: cadena 2
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\0');
+	}
+}
