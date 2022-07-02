80ebd81e13db1c717b76a4feccddc4161379080d
Giogap <giogap@gmail.com>
2022-07-02 16:02:31 -0700

    8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - check code
+ * @s: cadena 1
+ * @accept: cadena 2
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int acum1 = 0;
+
+	for (i = 0; i < size; i++);
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i = j)
+			{
+				acum1 = acum1 + a[i][j];
+			}	
+
+			
+
+}
