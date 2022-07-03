4778bb99fae8f862fa4dc224433b4cb3fbf262a9
Giogap <giogap@gmail.com>
2022-07-03 14:49:16 -0700

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _print_rev_recursion - Print strn reverse
+ * _strlen_recursion - check code
  *
- * @s: string to print reverse
+ * @s: string 
  *
  */
 
 int _strlen_recursion(char *s)
 {
-
-
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
