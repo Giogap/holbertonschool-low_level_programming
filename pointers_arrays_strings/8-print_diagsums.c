e7b6d6ab6ec6f2552013241b629f74019c36d5e3
Giogap <giogap@gmail.com>
2022-07-02 21:19:02 -0700

    8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - check code
@@ -12,6 +13,8 @@ void print_diagsums(int *a, int size)
 	int i;
 	int j;
 	int acum1 = 0;
+	int acum2 = 0;
+	int sum = 0;
 
 	for (i = 0; i < size; i++);
 	{
@@ -21,7 +24,14 @@ void print_diagsums(int *a, int size)
 			{
 				acum1 = acum1 + a[i][j];
 			}	
+		}
 
-			
-
+		for (j = 0; j < size; j++)
+		{
+			i = size - 1;
+			acum2 = acum2 + a[i][j];
+		}
+	}
+	sum = acum1 + acum2;
+	printf("%i, %i\n", acum1, acum2);
 }
