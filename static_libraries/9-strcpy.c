6926a05fce94bc7570ee9572fbc0d7c3d39fc537
Giogap <giogap@gmail.com>
2022-07-04 21:45:07 -0700

    0

--- static_libraries/9-strcpy.c
+++ static_libraries/9-strcpy.c
@@ -20,26 +20,4 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 
 	return (dest);
-#include "main.h"
-#include <string.h>
-
-/**
- * _strcpy - check the code
- * @dest: paste
- * @src: copy
- * Description: copy src to dest
- * Return: dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
 }
