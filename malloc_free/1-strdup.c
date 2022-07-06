c0b056c2b5106fceac2c75c16b20ea458b82b4e3
Giogap <giogap@gmail.com>
2022-07-06 13:39:04 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - check code
+ * @size: size array
+ * @c: char initialized
+ * Return: s or null
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+
+	if(str == 0)
+	{
+		return (NULL);
+	}
+
+
+}
