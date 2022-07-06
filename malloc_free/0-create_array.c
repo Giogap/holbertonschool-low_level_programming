6b21bcf7095665aa048331d6a073dd9002903d6a
Giogap <giogap@gmail.com>
2022-07-06 12:43:23 -0700

    0.1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * @size: size array
  * @c: char initialized
  * Return: s or null
- **/
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *s;
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
