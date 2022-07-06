3fda6b8ed6c925c04a9dc88f1e09a144983b8d88
Giogap <giogap@gmail.com>
2022-07-05 20:30:01 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,15 +13,15 @@ int main(int argc, char *argv[])
 {
 	int mult;
 
-	if (argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		mult = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", mult);
 		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 }
