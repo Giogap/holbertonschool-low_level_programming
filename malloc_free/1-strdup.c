d8327a724642b0c767dad50b506a7ac2b55cfffd
Giogap <giogap@gmail.com>
2022-07-06 16:47:42 -0700

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,20 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - check code
- * @size: size array
- * @c: char initialized
- * Return: s or null
+ * _strdup - check code
+ * @str: array
+ *
+ * Return: str2 or null
  */
 
 char *_strdup(char *str)
 {
 	int i;
+	int acum = 1;
+	char *str2;
 
 	if(str == 0)
 	{
 		return (NULL);
 	}
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		acum = acum + i;
+	}
 
+	str2 = malloc(sizeof(*str) * acum);
+
+	if (str2 == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str2[i] = str[i];
+	}
+	return (str2);
 }
