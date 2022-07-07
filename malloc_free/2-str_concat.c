9d7efee79f38639763d9672c3fddb842aac5d8a0
Giogap <giogap@gmail.com>
2022-07-06 21:27:34 -0700

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * str_concat - check code
+ * @s1: array 1
+ * @s2: array 2
+ * Return: array concat or null
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	int i;
+	int j;
+
+	if (!*s1 || !*s2)
+	{
+		return (NULL);
+	}
+
+	s3 = malloc(sizeof(*s3) * (strlen(s1) + strlen(s2) + 1));
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s3[i] = s1[i];
+		j = j + 1;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		s3[j] = s2[i];
+	}
+	return (s3);
+}
