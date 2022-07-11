4a1135ec514b21f8065dcee66378d943a36627d0
Giogap <giogap@gmail.com>
2022-07-11 11:56:16 -0700

    2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *name;
+
+	name = "2-main.c";
+	printf("%p\n", name);
+	return (0);
+}
