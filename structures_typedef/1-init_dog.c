43bb9bcc86a5800a5eb9e2d8bd9cc53b7c8517e9
Giogap <giogap@gmail.com>
2022-07-12 10:22:05 -0700

    3.4

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+  * init_dog - check code
+  * @d: array info dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog 
+  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
