4810b7a399036376743d0297ec632fc2bc352e85
Giogap <giogap@gmail.com>
2022-07-12 13:03:11 -0700

    2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * init_dog - check code
+  * @d: array info dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d = NULL)
+	{
+		printf (nil);
+	}
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	if (name = NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%s\n", d->name);
+	}
+	printf("%p\n", d->age)
+	printf("%s\n", d->owner)	
+}
