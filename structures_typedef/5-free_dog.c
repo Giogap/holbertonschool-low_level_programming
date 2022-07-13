cc06197b9fb6d7636f8d0e06ead373c2d00696d1
Giogap <giogap@gmail.com>
2022-07-12 22:33:56 -0700

    5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * new_dog - check code
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  * Return: dog or NULL
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		d = malloc(sizeof(*d));
+		free(d);
+	}
+}
