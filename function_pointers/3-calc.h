3da3f9a4dc8192e5915611327757fe36e2848c47
Giogap <giogap@gmail.com>
2022-07-14 20:45:46 -0700

    3.4

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,31 +1,6 @@
-#ifndef _CALC_H
-#define _CALC_H
+#ifndef CALCH
+#define CALCH
 
-/* importing */
-#include <stdio.h>
-#include <stdlib.h>
-
-/* types and structures */
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
-
-/* macros valores constantes */
-#define ERROR_MESSAGE ("Error\n")
-#define EXIT_BY_ZERO (100)
-#define EXIT_NULL_OPERATOR (99)
-#define EXIT_INVALID_ARG (98)
-#define FORMAT_RESULT ("%d\n")
-
-/* prototypes */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -33,16 +8,17 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-/* macros functions */
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 
-#define THROW_ERROR(CONDITIONAL, ERROR_TYPE, EXIT_CODE) \
-do {                                                    \
-		if (CONDITIONAL)                                \
-		{                                               \
-				printf(ERROR_TYPE);                     \
-				exit(EXIT_CODE);                        \
-		}                                               \
-} while (0)
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-
-#endif /* _CALC_H */
+#endif
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,23 +8,10 @@
  * Return: pointer op
  */
 
-int includes(char *string, char *substring)
-{
-	if (string == NULL || substring == NULL)
-		return (0);
-
-	if (*string == '\0')
-		return (1);
-
-	if (*string == *substring)
-		return (includes(string + 1, substring + 1));
-
-	return (0);
-}
-
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
