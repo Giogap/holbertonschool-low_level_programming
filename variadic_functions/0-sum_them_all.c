f69d1ff8ffe6087563cc94ff825349d2363cabaa
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-15 15:00:51 -0500

    0.6

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,18 +9,18 @@
  */
 
 int sum_them_all(const unsigned int n, ...)
-  {
+{
 	unsigned int i;
 	va_list listarg;
 	int suma = 0, arg;
 
-	va_start (listarg, n);
+	va_start(listarg, n);
 
 	for (i = 0; i < n; i++)
-      {
+	{
 		arg = va_arg(listarg, int);
 		suma = suma + arg;
-      }
-	va_end (listarg);
+	}
+	va_end(listarg);
 	return (suma);
 }
