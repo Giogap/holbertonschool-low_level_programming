006d2f9f4a9c03eb9075603644c3ebc18e321fe9
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-15 12:19:50 -0500

    0.3

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,1 +1,26 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *  - check code
+ * @
+ * @
+ */
+
+int sum_them_all(const unsigned int n, ...)
+  {
+    unsigned int i;
+    va_list listarg;
+    int suma = 0, arg;
+
+    va_start (listarg, n);
+
+    for (i = 0; i < n; i++)
+      {
+        arg = va_arg(listarg, int);
+        suma = suma + arg;
+      }
+
+    va_end(listarg);
+    ruturn (suma);
+}
