a7af3e9e707616ff7b5e79e5144f61e8e10fa83a
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-26 11:52:08 -0500

    0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	h = NULL;
+	return(nill);
+
+	list_t *h = hlist_t(h);
+	printf("%p", h);
+
+	return (0);
+}
