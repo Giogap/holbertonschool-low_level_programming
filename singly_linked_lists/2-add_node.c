959fe3a57f2bba03d6ce592a2d46bd3f56c00a43
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-27 13:46:25 -0500

    2.2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node - check code
+ * @head: first pointer
+ * @str: streng new element
+ * Return: new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *nnode =malloc(sizeof(list_t));
+
+	if (nnode == NULL)
+	{
+		return (NULL);
+	}
+
+	nnode->str = strdup(str);
+
+	nnode ->len = strlen(nnode->str);
+	nnode->next = *head;
+	*head = nnode;
+	return (nnode);
+}
