094d14e57179ff066f15f3f5bb9ff98e26641afb
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-28 17:54:14 -0500

    3.1

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,21 +6,26 @@
  * add_node - check code
  * @head: first pointer
  * @str: streng new element
- * Return: new node
+ * Return: h
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *nnode, *h = *head;
 
-	list_t *nnode;
+	nnode = malloc(sizeof(list_t));
 
-	while (head != NULL)
+	if (nnode == NULL)
 	{
-		head = head->next;
+		return (NULL);
 	}
 
-	nnode->len = strlen(nnode->str);
-	nnode->next = *head;
-	*head = nnode;
-	return (nnode);
+	nnode->str = strdup(str);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+	}
+	h->next = nnode;
+	return (h);
 }
