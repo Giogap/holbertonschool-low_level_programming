cb94d99674733fe7c0cc33859022766eae169f9e
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-29 08:07:38 -0500

    6.3

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -8,10 +8,16 @@
 int pop_listint(listint_t **head)
 {
 	listint *tem;
+	int n;
+
+	if (*head == NULL || head == NULL)
+	{
+		retern (0);
+	}
 
 	*tem = *head;
 	*head = *head->next;
 	n = tem->n;
 	free(tem);
-	
+	return (n);i
 }
