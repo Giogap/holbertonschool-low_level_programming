c448ff57f5717a096db5b53e570fac1c71ed6cf2
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-29 07:04:09 -0500

    6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+
+int pop_listint(listint_t **head)
+{
+	listint *tem;
+
+	tem = *head;
+	*head = *head->next;
+	tem = NULL;
+}
