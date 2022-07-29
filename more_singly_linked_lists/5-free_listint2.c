d4883ffdc634758c3320fcbc035aa6e760815379
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-29 06:04:10 -0500

    5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - check code
+ * @head: pointer
+ * Retunt: NULL;
+ */
+
+void free_listint2(listint_t **head)
+{
+	
+
+}
