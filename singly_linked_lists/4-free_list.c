6473f0723f89251e6ec1db827746b51f1080b21d
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-28 21:32:30 -0500

    4.2

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -3,25 +3,13 @@
 #include <stdlib.h>
 
 /**
- * list_len - check code
- * @h: pointer
- * Description: returns the number of elements
- * Return: acum
+ * free_list - check code
+ * @head: pointer
+ * Description: free
+ *
  */
 
 void free_list(list_t *head)
 {
-	int cont = 0;
-
-	while (*head != NULL)
-	{
-		cont = cont + 1;
-	}
-
-	head->str = strdup(str);
-
-	if (*head == NULL)
-	{
-		free(head);
-	}
+	free(head);
 }
