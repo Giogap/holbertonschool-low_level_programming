6761724828f771c48d200d60844adbde667c4fae
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-30 22:56:24 -0500

    7.1

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+es (23 sloc)  451 Bytes
+
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/**
+ * get_nodeint_at_index - check code
+ * @head: pointer
+ * @index: value
+ * Return: NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+
+
+
+}
