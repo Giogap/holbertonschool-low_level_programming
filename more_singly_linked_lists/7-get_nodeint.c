38e9b7ed75754ae9b4963f26d211c6eef89e6dcb
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-30 22:58:54 -0500

    7.3

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,3 @@
-es (23 sloc)  451 Bytes
-
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,8 +12,17 @@ es (23 sloc)  451 Bytes
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *currentNode = head;
+	unsigned int count = 0;
 
-
-
-
+	while (currentNode != NULL)
+	{
+		if (count == index)
+		{
+			return (currentNode);
+		}
+		count++;
+		currentNode = currentNode->next;
+	}
+	return (NULL);
 }
