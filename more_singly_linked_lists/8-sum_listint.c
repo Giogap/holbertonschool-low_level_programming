4ce8efaf40fd193ae059f6ef427baba58892aa49
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-30 23:28:08 -0500

    8.2

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -8,3 +8,16 @@
  * @head: pointer head
  * Return: sum
  */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+	printf("sum = %d", sum);
+}
