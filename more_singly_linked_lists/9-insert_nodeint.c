492b8d60f07ca984039d8d3c32d94e04ca7efbaa
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-30 23:37:09 -0500

    9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insert_nodeint_at_index - check code
+ * @head: pointer
+ * @idx: position Node
+ * @n: data
+ * Return: newNode or NULL;
+ */
