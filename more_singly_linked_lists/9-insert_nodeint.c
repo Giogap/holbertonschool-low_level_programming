c1983f69366efbd71d4cd236b738ff1391774922
giogap <giogap@P-GIOVANNI.localdomain>
2022-07-30 23:42:05 -0500

    9.1

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,3 +9,35 @@
  * @n: data
  * Return: newNode or NULL;
  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head, *newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+		newNode->next = temp;
+		*head = newNode;
+		return (*head);
+	}
+
+	while (idx > 1)
+	{
+		temp = temp->next;
+		idx--;
+		if (!temp)
+		{
+			free(newNode);
+			return (NULL);
+		}
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+	return (newNode);
+}
