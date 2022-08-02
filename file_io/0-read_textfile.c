a24b0906d1e50ae2664915cdb01f91b444900c16
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-02 14:31:52 -0500

    0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,22 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
 
 
 
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
 
+	fd = open("filename", O_WRONLY);
 
+	if (fd == -1)
+	{
+		return (0);
+	}
+	return (0);
 }
