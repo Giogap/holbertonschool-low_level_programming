7055201743d114df435ed42979a652cb062ecbd7
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 14:51:20 -0500

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,9 +1,36 @@
-
-
+#include "main.h"
+#include <stddef.h>
 
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fb, len= 0;
 
+	if (filename == NULL)
+	{
+		return (-1)
+	}
 
+	fb = open(filename, O_WRONLY | O_APPEN);
+
+	if (fb == -1)
+	{
+		return (-1)
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	while (text_content[len])
+	{
+		len++;
+	}
+
+	write(fb, text_content, len)
+
+	close(fb)
+
+	return (1);
 }
