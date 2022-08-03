b4adfba164d5de840e8276cc41fa18085363fa68
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 11:59:19 -0500

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,41 +17,34 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, lenwr;
 	char *buf;
-
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
-
 		if (fd == -1)
 		{
 			return (-1);
 		}
 		return (1);
 	}
-
 	while (text_content[len] != '\0')
 	{
 		len++;
 	}
 	buf = malloc(len * sizeof(char));
-
 	if (buf == NULL)
 	{
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 	{
 		return (-1);
 	}
 	lenwr = write(fd, text_content, len);
-
 	if (lenwr == -1)
 	{
 		return (-1);
