2f4793b9acd6fb0d907728d85650ad13e1c2e526
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 12:25:55 -0500

    betty 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,9 +8,9 @@
 
 /**
  * create_file - check code
- *
- *
- *
+ * @filname: name of file
+ * @text_content: string
+ * Return: 1 or -1
  */
 
 int create_file(const char *filename, char *text_content)
