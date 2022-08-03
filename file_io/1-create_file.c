283aeb9967af2eae9fa5b70fbf1a6832b1cc9ff4
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 00:14:19 -0500

    1.3

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 
 
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, lenwr;
@@ -54,5 +55,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
+	free(buf);
 	return (1);
 }
