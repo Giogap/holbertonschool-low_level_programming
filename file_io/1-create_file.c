eb74174ca8499ce895c29ce2e4ee83362acdc264
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 11:20:19 -0500

    1.9

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open("filename", O_CREAT, 0600);
+		fd = open(filename, O_CREAT, 0600);
 
 		if (fd == -1)
 		{
@@ -43,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
