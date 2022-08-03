3cb06a398a6072330cd9fd29db26a3cc90d177f4
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-03 11:02:30 -0500

    1.5

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,7 +4,12 @@
 #include "main.h"
 #include <fcntl.h>
 
-
+/**
+ * create_file - check code
+ *
+ *
+ *
+ */
 
 int create_file(const char *filename, char *text_content)
 {
@@ -19,41 +24,35 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fd = open("filename", O_CREAT, 0600);
-	}
-		
-	fd = open("filename", O_CREAT | O_WRONLY, 0600);
 
-	if (fd == -1)
-	{
-		return (-1);;
+		if (fd == -1)
+		{
+			return (-1);
+		}
 	}
 
 	while (text_content[len] != '\0')
 	{
 		len++;
 	}
-
 	buf = malloc(len * sizeof(char));
 
 	if (buf == NULL)
 	{
 		return (-1);
 	}
-
-	fd = open("filename", O_CREAT | O_WRONLY | O_TRUNC);
+	fd = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
-
 	lenwr = write(fd, text_content, len);
 
 	if (lenwr == -1)
 	{
 		return (-1);
 	}
-
 	close(fd);
 	free(buf);
 	return (1);
