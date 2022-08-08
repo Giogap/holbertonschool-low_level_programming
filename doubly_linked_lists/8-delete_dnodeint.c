1dcaeff15b65b520ae1a2a5fca364a94b3cc41c8
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-08 14:07:53 -0500

    8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	dlistint_t *temp2 = NULL;
+	int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+	}
+	else
+	{
+		for (i = 0; (i < index) && (temp->next != NULL); i++)
+			temp = temp->next;
+		if (!temp)
+			return (-1);
+		if (index > i)
+			return (-1);
+		temp2 = temp->prev;
+		temp2->next = temp->next;
+		temp->next->prev = temp2;
+	}
+	free(temp);
+	return (1);
+}
