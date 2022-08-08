8b91a31d3e7f6ebc7df65f5ea4096526f2886c12
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-08 18:35:42 -0500

    update 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,6 +7,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *nnode, *temp = *h;
 	unsigned int index, cont = 0;
 
-	nnode = malloc(sizeof(dlintint_t));
+	nnode = malloc(sizeof(dlistint_t));
 
+	if (nnode == NULL)
+	{
+		return (NULL);
+	}
+
+	nnode->n = n;
+
+	if (idx == 0)
+	{
+		nnode->prev = NULL;
+		nnode->next = *h;
+
+		if (*h)
+		{
+			(*h)->prev = nnode;
+		}
+		*h = nnode;
+		return (*h);
+	}
+	index = idx - 1;
+
+	while (temp && cont != index)
+	{
+		cont = cont + 1;
+		temp = temp->next;
+	}
+	return (0);
 }
