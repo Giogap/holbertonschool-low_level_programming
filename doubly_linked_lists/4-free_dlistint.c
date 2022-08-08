5a4600e966936bf2752c3f92a906ead2fc019918
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-08 11:19:28 -0500

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *nnode;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		nnode = head;
+		head = nnode->next;
+		free(nnode);
+	}
+	head = NULL;
+}
