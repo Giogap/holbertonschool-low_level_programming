c8cf10c70900f0cc09ae04196f641499ff6d4894
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-31 18:11:25 -0500

    update 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,37 +10,21 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item = NULL, *current_item = NULL;
-	unsigned long int index = 0;
+	hash_node_t *nnode = NULL;
 
 	if (!ht || !key || !value)
-		return (0);
-	if (strlen(key) == 0)
-		return (0);
-	item = malloc(sizeof(hash_node_t));
-	if (item == NULL)
-		return (0);
-	item->next = NULL;
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] != NULL)
 	{
-		current_item = ht->array[index];
-		while (current_item)
-		{
-			if (strcmp(current_item->key, keyDup) == 0)
-			{
-				free(ht->array[index]->value);
-				free(keyDup);
-				free(item);
-				return (1);
-			}
-			current_item = current_item->next;
-		}
-		current_item = ht->array[index];
-		item->next = current_item;
-		ht->array[index] = item;
+		return (0);
 	}
-	else
-		ht->array[index] = item;
+
+	nnode = malloc(sizeof(hash_node_t));
+
+	if (nnode == NULL)
+	{
+		return (0);
+	}
+
+
+
 	return (1);
 }
