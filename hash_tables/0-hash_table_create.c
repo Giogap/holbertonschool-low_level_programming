dbe9bd0860ca22a7bc5ff68fb3082aa06f44929b
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-30 19:39:46 -0500

    upd 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,5 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return(NULL);
 	}
 
-	return (0);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	return (ht);
 }
