a2cba09e7a3021595fc7785245bf842a3c7f45d7
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-31 01:11:48 -0500

    update 2 and betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,10 +1,12 @@
 #include "hash_tables.h"
 
 /**
+ * key_index - check code
  *
+ * @key: key
+ * @size: size hash
  *
- *
- *
+ * Return: index
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
