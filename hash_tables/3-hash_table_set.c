ab1db059bafb3f60f792525af377ca7d2cb556c7
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-31 22:25:41 -0500

    update 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,6 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL && ht->array[index]->value == 0)
 	{
+		ht->array[index]->value = strdup(value);
 		return (1);
 	}
 
