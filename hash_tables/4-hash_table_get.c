b5d91056a7adcb03c7aeababf9248189e002bbc1
giogap <giogap@P-GIOVANNI.localdomain>
2022-09-01 01:12:50 -0500

    4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *hnode = NULL;
+	unsigned long int index;
+
+	if (!ht || !key || !key[0])
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	hnode = ht->array[index];
+
+	while (hnode != NULL)
+	{
+		if (!strcmp(hnode->key, key))
+			return (hnode->value);
+
+		hnode = hnode->next;
+	}
+
+	return (NULL);
+}
