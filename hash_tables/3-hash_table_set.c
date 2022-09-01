56a25b05f302111aa921817cb70725a1ca114bc4
giogap <giogap@P-GIOVANNI.localdomain>
2022-09-01 00:27:38 -0500

    update 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,24 +11,43 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *nnode = NULL;
+	char *value_dup = NULL, *key_dup = NULL;
+	hash_node_t *new_node = NULL, *tmp_node = NULL;
 
-	if (!ht || !key)
+	if (!ht || !key || !value)
+		return (0);
+	if (strlen(key) == 0)
 		return (0);
 
-	nnode = malloc(sizeof(hash_node_t));
-	if (!nnode)
+	value_dup = strdup(value);
+	key_dup = strdup(key);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 		return (0);
-
-	index = key_index((unsigned const char *)key, ht->size);
-
-	nnode->key = strdup(key);
-	nnode->value = strdup(value);
-
-	if (ht->array[index] && strcmp(key, ht->array[index]->key) != 0)
-		nnode->next = ht->array[index];
-
-	ht->array[index] = nnode;
-
+	new_node->key = key_dup;
+	new_node->value = value_dup;
+	new_node->next = NULL;
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL)
+	{
+		tmp_node = ht->array[index];
+		while (tmp_node)
+		{
+			if (strcmp(tmp_node->key, key_dup) == 0)
+			{
+				free(ht->array[index]->value);
+				ht->array[index]->value = value_dup;
+				free(key_dup);
+				free(new_node);
+				return (1);
+			}
+			tmp_node = tmp_node->next;
+		}
+		tmp_node = ht->array[index];
+		new_node->next = tmp_node;
+		ht->array[index] = new_node;
+	}
+	else
+		ht->array[index] = new_node;
 	return (1);
 }
