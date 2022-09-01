1a9b7099151eeaed2e31e61c1d163aa33dff05a6
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-31 22:34:40 -0500

    update 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,8 +29,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-
-
+	nnode->key = strdup(key);
+	nnode->value = strdup(value);
+	nnode->next = ht->array[index];
 
 	return (1);
 }
