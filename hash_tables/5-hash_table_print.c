b4e6051622357a3731fc9d66d7c75aac3f314714
giogap <giogap@P-GIOVANNI.localdomain>
2022-09-01 22:15:33 -0500

    5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - check code
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *item = NULL;
+	int flag = 0;
+
+	if (!ht || !ht->array)
+		return;
+}
