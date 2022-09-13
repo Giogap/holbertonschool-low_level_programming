64134b33424bec38f442c1f8c1e2bf61cc7c5c09
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-13 15:33:31 -0500

    update 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,18 +1,32 @@
 #include "search_algos.h"
 
 /**
- * print_array - check code
+ * binary_search - check code
  * @array: array
- * @low: value low
- * @high: value high
+ * @size: size array
+ * @value: key
+ * Return: always value or -1
  */
 
-void print_array(int *array, int low, int high)
+int binary_search(int *array, size_t size, int value)
 {
-	int i;
+	int low = 0, high = size - 1, central;
+	int valueCentral;
 
-	printf("Searching in array: ");
-	for (i = low; i < high; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+	if (size == 0 || array == NULL || value == 0)
+		return (-1);
+
+	while (low <= high)
+	{
+		print_array(array, low, high);
+		central = (low + high) / 2;
+		valueCentral = array[central];
+		if (value == valueCentral)
+			return (central);
+		else if (value < valueCentral)
+			high = central - 1;
+		else
+			low = central + 1;
+	}
+	return (-1);
 }
