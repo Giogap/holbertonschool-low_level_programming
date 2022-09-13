e2e82c4f11cac779211b4f9d3acb25c2c6a0d86d
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-13 15:34:37 -0500

    update 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,23 @@
 #include "search_algos.h"
 
 /**
+ * print_array - check code
+ * @array: array
+ * @low: value low
+ * @high: value high
+ */
+
+void print_array(int *array, int low, int high)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = low; i < high; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
  * binary_search - check code
  * @array: array
  * @size: size array
